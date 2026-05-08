0a6135b66a62606ad1cd852124c6ce5584612f05
hoannm1 <hoannm1@vnpt.vn>
2026-05-09 00:44:41 +0700

    chỉnh sửa folder

--- Backend/Social.Services/Social.EmailServices/Interface/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Social.EmailServices.Interface
-{
-    public interface IEmailService
-    {
-        Task SendOtpEmailAsync(string toEmail, string otpCode);
-    }
-}
--- Backend/Socical.Services/Social.AuthServices/Interface/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Social.AuthServices.Interface
-{
-    public interface IAuthService
-    {
-        Task<(bool Success, string Message)> RegisterAsync(string email, string password);
-        Task<(bool Success, string Message)> VerifyEmailAsync(string email, string otpCode);
-        Task<(bool Success, string Message)> ResendOtpAsync(string email);
-    }
-}
